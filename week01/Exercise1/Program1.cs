db79b553a4a9c7667086e29e723460ed2e834949
beatr256 <hapolot@byupathway.edu>
2025-10-31 02:24:13 -0700

    push

--- week01/Exercise1/Program1.cs
+++ week01/Exercise1/Program1.cs
@@ -7,17 +7,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("What is your first name?");
-         string first = Console.ReadLine();
-
+        string first = Console.ReadLine();
 
         Console.Write("What is your last name? ");
         string last = Console.ReadLine();
-        
+
         Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 
-    private string GetDebuggerDisplay()
+    private static string GetDebuggerDisplay()
     {
-        return ToString();
+        return "Program Debugger Display";
     }
 }
